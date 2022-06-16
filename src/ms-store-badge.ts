a0a8ab6a949a59061c1f78b282d492aeb8150c6e
Justin Lau <justinlau@gatech.edu>
2022-06-16 11:47:35 -0700

    Remove unnecessary code

--- src/ms-store-badge.ts
+++ src/ms-store-badge.ts
@@ -145,22 +145,7 @@
         cursor: pointer;
         box-shadow: 0 12px 40px 20px rgba(0, 0, 0, 0.05);
       }
-
-      img:hover:after {
-        content: "";
-        position: absolute;
-        left: 0;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        z-index: -1;
-        width: 100%;
-        height: 100%;
-        opacity: 0;
-        box-shadow: 0 12px 40px 2px rgba(0, 0, 0, 0.05);
-        transition: 0.35s ease;
-      }
-
+      
       img.small {
         max-height: 52px;
       }
